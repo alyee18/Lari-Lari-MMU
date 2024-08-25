2b9136557dfcce0116cd5a9267fd158d1ea3b8f7
Chong Jin Voon <jinvoon2112@gmail.com>
2024-08-26 01:34:42 +0800

    have some error

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import sqlite3
 from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
-app.secret_key = "your_secret_key"  # Necessary for session management
+app.secret_key = "your_secret_key"
 
 @app.route("/")
 def home():
